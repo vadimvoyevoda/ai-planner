755b8977f79ae86815ffbaad04b754f415f644ea
Vadim <vadim.voievoda@traveltech.pl>
2025-05-14 20:37:56 +0200

    fixes

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -2,7 +2,8 @@ import * as React from "react"
 import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-import { ButtonProps, buttonVariants } from "@/components/ui/button"
+import { buttonVariants } from "@/components/ui/button"
+import type { ButtonProps } from "@/components/ui/button"
 
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
   <nav
@@ -43,6 +44,7 @@ const PaginationLink = ({
   className,
   isActive,
   size = "icon",
+  children,
   ...props
 }: PaginationLinkProps) => (
   <a
@@ -55,7 +57,9 @@ const PaginationLink = ({
       className
     )}
     {...props}
-  />
+  >
+    {children || <span className="sr-only">Page</span>}
+  </a>
 )
 PaginationLink.displayName = "PaginationLink"
 
